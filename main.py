24fc12a2f2f9c2827fec2d3c536a166477c67446
aakhomchenko <homchenko_aa@nrcki.ru>
2025-04-14 09:18:09 +0300

    Добавлено вычисление азумута и построение таблиц

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 from parser import DataParser
 import igs
 import heapq
+import math
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
+from prettytable import PrettyTable
 FILENAME = "data/leij_1-10.dat"
 DELTA_T = 30
 SATELLITES = []
 
+
+def calculate_azimuth(site_cords: tuple, sat_cords: tuple):
+    x_site, y_site, z_site = site_cords
+    x_sat, y_sat, z_sat = sat_cords
+
+    delta_x = x_sat - x_site
+    delta_y = y_sat - y_site
+    delta_z = z_sat - z_site
+
+    azimuth = math.atan(delta_x/delta_y)
+    return azimuth
+
+
 if __name__ == '__main__':
     site = igs.IgsSite(FILENAME.split('/')[1][0:4])
     print(site)
+
+    site_cords = (site.x, site.y, site.z)
 
     data_parser = DataParser(FILENAME)
     satellites = data_parser.epoch_to_sat_dict()
@@ -29,25 +45,33 @@ if __name__ == '__main__':
     sats = []
     epochs = []
 
-
     for sat_id, sat in sat_data.items():
         x, y, z, elev = sat['x_sat'].values, sat['y_sat'].values, sat['z_sat'].values, sat['elevation'].values
         epoch = sat['epoch'].values[0:-1]
         V, W = [], []
 
+        table = PrettyTable(["Epoch", "Linear velocity", "Angular velocity"])
+        table.title = f"Satellite {sat_id}"
 
         for i in range(len(x)-1):
             dx = (x[i+1] - x[i]) / DELTA_T
             dy = (y[i+1] - y[i]) / DELTA_T
             dz = (z[i+1] - z[i]) / DELTA_T
-            dw = elev[i+1] - elev[i] / DELTA_T
-            V.append(np.sqrt(dx**2 + dy**2 + dz**2))
-            W.append(dw)
+            v = np.sqrt(dx**2 + dy**2 + dz**2)
+            V.append(v)
+
+            d_theta = (elev[i+1] - elev[i]) / DELTA_T
+            d_azim = (calculate_azimuth(site_cords, (x[i+1], y[i+1], z[i+1])) - calculate_azimuth(site_cords, (x[i], y[i], z[i]))) / DELTA_T
+            w = np.sqrt(d_theta**2 + d_azim**2)
+            W.append(w)
+
+            table.add_row([epoch, v, w])
 
         velocities.append(V)
         angular_velocities.append(W)
         sats.append(sat_id)
         epochs.append(epoch)
+        print(table)
 
     plt.figure(figsize=(12, 12))
     for i in range(len(sats)):
@@ -58,6 +82,7 @@ if __name__ == '__main__':
     plt.legend(loc='best')
     plt.suptitle(f"Velocities of Satellites")
     plt.savefig("figures/velocities.png")
+    plt.show()
 
     plt.figure(figsize=(12, 12))
     for i in range(len(sats)):
@@ -68,3 +93,4 @@ if __name__ == '__main__':
     plt.legend(loc='best')
     plt.suptitle(f"Angular velocities of Satellites")
     plt.savefig("figures/angular_velocities.png")
+    plt.show()
